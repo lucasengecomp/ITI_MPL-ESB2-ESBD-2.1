c774986f6615a4d22af8b423e1226c30855379d8
Lucas Castro <lucasengecomp@gmail.com>
2021-04-28 21:28:31 -0300

    Teste02: para valores unitarios

--- Roman_samples02.py
+++ Roman_samples02.py
@@ -1,4 +1,18 @@
 class RomanSamples:
+
 	@staticmethod
-	def RomanToInt(str):
-		return 1
+	def RomanToInt(str):		
+		romanCaracter = {
+			"I" : 1,
+			"V" : 5,
+			"X" : 10,
+			"L" : 50,
+			"C" : 100,
+			"D" : 500,
+			"M" : 1000
+		}
+
+		if str in romanCaracter.keys():
+			return romanCaracter[str]
+		return []
+
--- test02.py
+++ test02.py
@@ -5,3 +5,21 @@ class RomanToIntTest(unittest.TestCase):
 
     def test_roman01(self):
     	self.assertEqual(RomanSamples.RomanToInt("I"), 1)
+
+    def test_roman02(self):
+    	self.assertEqual(RomanSamples.RomanToInt("V"), 5)
+
+    def test_roman03(self):
+    	self.assertEqual(RomanSamples.RomanToInt("X"), 10)
+
+    def test_roman04(self):
+    	self.assertEqual(RomanSamples.RomanToInt("L"), 50)
+
+    def test_roman05(self):
+    	self.assertEqual(RomanSamples.RomanToInt("C"), 100)
+
+    def test_roman06(self):
+    	self.assertEqual(RomanSamples.RomanToInt("D"), 500)
+
+    def test_roman07(self):
+    	self.assertEqual(RomanSamples.RomanToInt("M"), 1000)
